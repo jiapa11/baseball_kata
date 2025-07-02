31100285e947660e89dcfe648191980ca2d985d0
noname <none@none.com>
2025-07-02 13:15:08 +0900

    Added handling for invalid character in user input

--- baseball_kata/baseball.cpp
+++ baseball_kata/baseball.cpp
@@ -10,5 +10,11 @@ public:
         if (string.length() != 3) {
             throw std::length_error("Must be exactly three letters");
         }
+
+        for (const char &ch : string) {
+            if (ch < '0' || ch > '9') {
+                throw std::invalid_argument("Must contain only numbers ");
+            }
+        }
     }
 };
--- baseball_kata/baseball_test.cpp
+++ baseball_kata/baseball_test.cpp
@@ -6,6 +6,11 @@ TEST(BaseballGame, ThrowExceptionInvalidInputLength) {
     EXPECT_THROW(game.guess(string{ "12" }), length_error);
 }
 
+TEST(BaseballGame, ThrowExceptionInvalidChar) {
+    Baseball game;
+    EXPECT_THROW(game.guess(string{ "12s" }), invalid_argument);
+}
+
 int main() {
     ::testing::InitGoogleTest();
     return RUN_ALL_TESTS();
