40d0ba9c096453888e68195e0c1a287bcbc4764d
noname <none@none.com>
2025-07-02 13:09:07 +0900

    Invalid input length handling implementation

--- baseball_kata/baseball.cpp
+++ baseball_kata/baseball.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+using std::string;
+
 class Baseball {
 public:
-
+    void guess(const string &string) {
+        if (string.length() != 3) {
+            throw std::length_error("Must be exactly three letters");
+        }
+    }
 };
