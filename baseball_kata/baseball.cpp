dda07d5af3fcf1a49c732a472e5e7f7a4cba1fc3
noname <none@none.com>
2025-07-02 14:57:42 +0900

    Added the rest-cases

--- baseball_kata/baseball.cpp
+++ baseball_kata/baseball.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 using std::string;
 
 struct GuessResult {
-    bool solved;
+    bool allCorrect;
     int strikes, balls;
 };
 
 class Baseball {
 public:
-    explicit Baseball(const std::string& answer) : answer{ answer } {};
+    explicit Baseball(const std::string& answer) : answer{ answer } 
+    {
+        for (int position = 0; position < LENGTH_OF_GUESS; position++) {
+            int num = answer[position] - '0';
+            numberToPosition[num] = position;
+        }
+    }
 
     GuessResult guess(const std::string &userGuess) {
         assertInvalidArgument(userGuess);
-        if (answer == userGuess) return { true, 3, 0 };
+        if (answer == userGuess) return ALL_CORRECT;
+        return evaluate(userGuess);
     }
 
-private:
+private:    
+    const static int LENGTH_OF_GUESS = 3;
     std::string answer;
+    std::unordered_map<int, int> numberToPosition{};
+    const struct GuessResult ALL_CORRECT = {true, 3, 0};
 
     void assertInvalidArgument(const std::string& userGuess)
     {
-        if (userGuess.length() != 3) {
+        if (userGuess.length() != LENGTH_OF_GUESS) {
             throw std::length_error("Must be exactly three letters");
         }
 
@@ -32,17 +44,34 @@ private:
             throw std::invalid_argument("Must contain only numbers ");
         }
 
-        if (isNumbersDuplicated(userGuess)) {
+        if (hasDuplicateNumber(userGuess)) {
             throw std::invalid_argument("Each number in input must be unique");
         }
     }
 
-    bool isNumbersDuplicated(const std::string& userGuess) {
+    bool hasDuplicateNumber(const std::string& userGuess) {
         if (userGuess[0] == userGuess[1] ||
             userGuess[0] == userGuess[2] ||
-            userGuess[1] == userGuess[3]) {
+            userGuess[1] == userGuess[2]) {
             return true;
         }
         return false;
     }
+
+    const GuessResult& evaluate(const std::string& userGuess)
+    {
+        int strikes{ 0 }, balls{ 0 };
+        for (int i = 0; i < LENGTH_OF_GUESS; i++) {
+            int num = userGuess[i] - '0';
+            auto search = numberToPosition.find(num);
+            if (search == numberToPosition.end()) continue;
+            else {
+                if (i == search->second) strikes++;
+                else balls++;
+            }
+        }
+
+        return { false, strikes, balls };
+    }
+
 };
--- baseball_kata/baseball_test.cpp
+++ baseball_kata/baseball_test.cpp
@@ -4,13 +4,21 @@
 class BaseballGameFixture : public testing::Test {
 public:
     Baseball game{ "123" };
-    void assertInvalidArgument(string userGuess) {
+    void assertInvalidArgument(const string &userGuess) {
         try {
             game.guess(userGuess);
             FAIL();
         }
         catch (const std::exception& e) {
         }
+    }
+
+    void assertValidArgument(GuessResult expected, const string &userGuess) {
+        GuessResult result = game.guess(userGuess);
+
+        EXPECT_EQ(expected.allCorrect, result.allCorrect);
+        EXPECT_EQ(expected.strikes, result.strikes);
+        EXPECT_EQ(expected.balls, result.balls);
     }
 };
 
@@ -23,14 +31,22 @@ TEST_F(BaseballGameFixture, ThrowExceptionInvalidInput) {
     assertInvalidArgument(string{ "121" });
 }
 
-TEST_F(BaseballGameFixture, ReturnValidResult) {
-    GuessResult result = game.guess("123");
-
-    EXPECT_TRUE(result.solved);
-    EXPECT_EQ(3, result.strikes);
-    EXPECT_EQ(0, result.balls);    
+TEST_F(BaseballGameFixture, AllCorrect) {
+    assertValidArgument({ true,3,0 }, "123");
 }
 
+TEST_F(BaseballGameFixture, AllWrong) {
+    assertValidArgument({ false,0,0 }, "456");
+}
+
+TEST_F(BaseballGameFixture, PartiallyCorrect) {
+    assertValidArgument({ false,2,0 }, "923");
+    assertValidArgument({ false,1,2 }, "213");
+    assertValidArgument({ false,1,2 }, "321");
+    assertValidArgument({ false,1,2 }, "321");
+    assertValidArgument({ false,1,0 }, "184");
+    assertValidArgument({ false,2,0 }, "125");
+}
 
 int main() {
     ::testing::InitGoogleTest();
